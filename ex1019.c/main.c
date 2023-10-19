94ed81e841068ef58e61475ac12ada8ba140bb43
godhyzzang <amksh0914@gmail.com>
2023-10-19 15:20:58 +0900

    7회차 실습 1 : 변수 범위
    7회차 실습 1 : 변수 범위

---
+++ ex1019.c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int flag = 1;
+	int y;
+	while (flag != 0) {
+		//static int y; //why static didn't work?
+		y = 3;
+		flag = 0;
+	
+	
+	}
+	y = 4;
+	return 0;
+
+}
