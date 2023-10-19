a1c42ddde0f63fd0ab2518c7057adb7b73e65f44
godhyzzang <amksh0914@gmail.com>
2023-10-19 15:51:47 +0900

    7회차 실습 3 : 매개변수
    7회차 실습 3 : 매개변수

--- ex1019.c/main.c
+++ ex1019.c/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-
-int main(void) {
-
-	int i;
-
-	for (i = 0; i < 5; i++) {
-		static int temp = 1; 
-		printf("temp=%d\n", temp);
-		temp++;
-	}
+int main(void)
+{
+	//int i;//initialize is required
+	int i = 10;
+	printf("함수 호출전 i = % d\n", i);
+	i =inc(i);
+	printf("함수 호출후 i = % d\n", i);
 	return 0;
-
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
